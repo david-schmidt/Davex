4903c10982567902bd0b74b5cbd8ffc5631e394c
Dave Lyons <dlyons@lyons42.com>
2020-08-19 20:23:58 -0700

    Add a lame brute-force shell script to build the indexed.help file. I imagine this should be something different to integrate with ant. Note that it's not only a loop over all the help/* files, because about 18 of them have an additional alias for the help index.

---
+++ build/build-help.sh
@@ -0,0 +1,85 @@
+./index.py create indexed.help 1400
+./index.py add indexed.help ../doc/help/Alias
+./index.py add indexed.help ../doc/help/BList
+./index.py add indexed.help ../doc/help/BRun
+./index.py add indexed.help ../doc/help/Boot
+./index.py add indexed.help ../doc/help/Bye quit
+./index.py add indexed.help ../doc/help/Cat ls
+./index.py add indexed.help ../doc/help/Cls home
+./index.py add indexed.help ../doc/help/CmdLine
+./index.py add indexed.help ../doc/help/Combine
+./index.py add indexed.help ../doc/help/ConP
+./index.py add indexed.help ../doc/help/Config
+./index.py add indexed.help ../doc/help/Copy cp
+./index.py add indexed.help ../doc/help/Create mkdir
+./index.py add indexed.help ../doc/help/DOSCat
+./index.py add indexed.help ../doc/help/DT
+./index.py add indexed.help ../doc/help/DU
+./index.py add indexed.help ../doc/help/Date
+./index.py add indexed.help ../doc/help/Delete rm
+./index.py add indexed.help ../doc/help/Deschw
+./index.py add indexed.help ../doc/help/Dev
+./index.py add indexed.help ../doc/help/Dump
+./index.py add indexed.help ../doc/help/Echo  e
+./index.py add indexed.help ../doc/help/Eject ej
+./index.py add indexed.help ../doc/help/Equal "="
+./index.py add indexed.help ../doc/help/Err
+./index.py add indexed.help ../doc/help/Exec "<"
+./index.py add indexed.help ../doc/help/Expand
+./index.py add indexed.help ../doc/help/FFind
+./index.py add indexed.help ../doc/help/FP
+./index.py add indexed.help ../doc/help/FType
+./index.py add indexed.help ../doc/help/Filetype
+./index.py add indexed.help ../doc/help/Find
+./index.py add indexed.help ../doc/help/GSBuff
+./index.py add indexed.help ../doc/help/Hdr
+./index.py add indexed.help ../doc/help/Help "?"
+./index.py add indexed.help ../doc/help/IW2
+./index.py add indexed.help ../doc/help/Index
+./index.py add indexed.help ../doc/help/Info
+./index.py add indexed.help ../doc/help/Init
+./index.py add indexed.help ../doc/help/Lock
+./index.py add indexed.help ../doc/help/MX80
+./index.py add indexed.help ../doc/help/ModChk
+./index.py add indexed.help ../doc/help/Mon
+./index.py add indexed.help ../doc/help/Move
+./index.py add indexed.help ../doc/help/Num
+./index.py add indexed.help ../doc/help/Online o
+./index.py add indexed.help ../doc/help/Origin or
+./index.py add indexed.help ../doc/help/Over
+./index.py add indexed.help ../doc/help/PG more
+./index.py add indexed.help ../doc/help/PSet
+./index.py add indexed.help ../doc/help/PType
+./index.py add indexed.help ../doc/help/Pathnames path
+./index.py add indexed.help ../doc/help/Prefix cd
+./index.py add indexed.help ../doc/help/Prot
+./index.py add indexed.help ../doc/help/RAM3
+./index.py add indexed.help ../doc/help/Rename
+./index.py add indexed.help ../doc/help/Rep
+./index.py add indexed.help ../doc/help/Scan
+./index.py add indexed.help ../doc/help/SetDate
+./index.py add indexed.help ../doc/help/SetStart
+./index.py add indexed.help ../doc/help/Shareware '$'
+./index.py add indexed.help ../doc/help/Size
+./index.py add indexed.help ../doc/help/Split
+./index.py add indexed.help ../doc/help/Spool
+./index.py add indexed.help ../doc/help/Strings
+./index.py add indexed.help ../doc/help/SysAlias
+./index.py add indexed.help ../doc/help/TR
+./index.py add indexed.help ../doc/help/Tail
+./index.py add indexed.help ../doc/help/Top
+./index.py add indexed.help ../doc/help/Topics
+./index.py add indexed.help ../doc/help/Touch
+./index.py add indexed.help ../doc/help/Type
+./index.py add indexed.help ../doc/help/Unlock
+./index.py add indexed.help ../doc/help/Up "\\"
+./index.py add indexed.help ../doc/help/Update
+./index.py add indexed.help ../doc/help/VRestore
+./index.py add indexed.help ../doc/help/VStore
+./index.py add indexed.help ../doc/help/Version
+./index.py add indexed.help ../doc/help/ViewDHR
+./index.py add indexed.help ../doc/help/ViewHR
+./index.py add indexed.help ../doc/help/WC
+./index.py add indexed.help ../doc/help/Wait
+./index.py add indexed.help ../doc/help/What
+./index.py add indexed.help ../doc/help/como ">"
